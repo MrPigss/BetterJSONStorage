defc19932c36be343bfb13cb96882eb1a70d2203
Thomas-Howest <thomas.eeckhout@student.howest.be>
2022-01-21 19:39:52 +0100

    added more effient storage

--- src/betterJSONStorage/BetterJSONStorage.py
+++ src/betterJSONStorage/BetterJSONStorage.py
@@ -1,3 +1,5 @@
+from io import BytesIO
+from threading import Thread
 from orjson import loads, dumps
 from blosc import compress, decompress
 from tinydb import Storage
@@ -35,3 +37,51 @@ class BetterJSONStorage(Storage):
         self._handle.flush()
         os.fsync(self._handle.fileno())
         self._handle.truncate()
+
+
+class testStorage(Storage):
+
+    class AsyncWriter(Thread):
+        def __init__(self, filename, data):
+            Thread.__init__(self, daemon=True)
+            self.filename = filename
+            self.data = data
+
+        def run(self):
+            with open(self.filename, 'wb') as f:
+                f.write(compress(self.data))
+
+    def __init__(self, path: str, access_mode='r+', create_dirs=False,**kwargs):
+        self._kwargs = kwargs
+        self._path = path
+
+        if any([character in access_mode for character in ('+', 'w', 'a')]):
+            touch(path, create_dirs)
+
+        # only open and decompress the file from storage once.
+        # then read and put the data in memory
+        with open(path, mode='rb') as handle:
+            self._handle = BytesIO(decompress(handle.read()))
+
+    # close the memory buffer
+    def close(self):
+        self._handle.close()
+
+    # only read from memorybuffer
+    def read(self):
+        self._handle.seek(0,2) # go to the end of the file
+        if not self._handle.tell(): return None # if tell returns zero file is empty
+        return loads(self._handle.getvalue()) # read from the buffer
+
+
+    def write(self, data):
+        serialized = dumps(data, **self._kwargs) # serialize data from table
+        self._handle.seek(0) #got to the start of the 'file'
+        self._handle.write(serialized) # write the serialized file
+        self._handle.truncate() # truncate in case the 'file' got smaller
+
+        # since everything has been done in memory it's not on disk yet
+        # compress and write to disk in a seperate daemon thread
+        # so we don't block the current one
+        self.AsyncWriter(self._path, serialized).start()
+
