62e72959f47b7b886fd84e9a14705f3122c96a44
Thomas-Howest <thomas.eeckhout@student.howest.be>
2022-01-26 17:42:19 +0100

    descriptors

--- BetterJSONStorage/BetterJSONStorage.py
+++ BetterJSONStorage/BetterJSONStorage.py
@@ -7,21 +7,52 @@ from orjson import dumps, loads
 from tinydb import Storage
 
 
-class Singleton(object):
-    _paths = []
+class Access_mode:
+    def __set_name__(self, owner, name):
+        self.private_name = f"_{name}"
 
-    def __new__(class_, path, *args, **kwargs):
-        h = hash(path)
-        paths = class_._paths
-        if h in paths:
-            raise AttributeError(
-                f'A BetterJSONStorage object already exists with path < "{path}" >'
+    def __set__(self, obj, value):
+        if not value in ("r", "r+"):
+            obj.close()
+            raise AttributeError(f'access_mode is not one of ("r", "r+"), :{value}')
+        setattr(obj, self.private_name, value)
+
+    def __get__(self, obj, objtype=None):
+        return getattr(obj, self.private_name)
+
+
+class FilePath:
+    def __set_name__(self, owner, name):
+        self.private_name = f"_{name}"
+
+    def __set__(self, obj, path):
+        setattr(obj, self.private_name, path)
+        if not isinstance(path, Path):
+            obj.close()
+            raise TypeError("path is not an instance of pathlib.Path")
+
+        if not path.exists():
+            if obj._access_mode == "r":
+                obj.close()
+                raise FileNotFoundError(
+                    f"""File can't be found, use access_mode='r+' if you wan to create it.
+                        Path: <{path.absolute()}>,
+                        """
+                )
+            path.parent.mkdir(parents=True, exist_ok=True)
+            path.touch()
+
+        if not path.is_file():
+            obj.close()
+            raise FileNotFoundError(
+                f"""path does not lead to a file: <{path.absolute()}>."""
             )
-        class_._paths.append(h)
-        return object.__new__(class_)
+
+    def __get__(self, obj, objtype=None):
+        return getattr(obj, self.private_name)
 
 
-class BetterJSONStorage(Storage, Singleton):
+class BetterJSONStorage(Storage):
     """
     A class that represents a storage interface for reading and writing to a file.
 
@@ -58,45 +89,29 @@ class BetterJSONStorage(Storage, Singleton):
     If the directory specified in `path` does not exist it will only be created if access_mode is set to `'r+'`.
     """
 
-    def __del__(self):
-        if (h := hash(self._path)) in (p := self.__class__._paths):
-            p.remove(h)
+    _paths = []
+    _access_mode = Access_mode()
+    _path = FilePath()
 
     def __init__(self, path: Path = Path(), access_mode: str = "r", **kwargs):
+        self._access_mode = access_mode
         self._path = path
-
-        if not isinstance(path, Path):
-            self.close()
-            raise TypeError("path is not an instance of pathlib.Path")
-
-        if not access_mode in ("r", "r+"):
-            self.close()
-            raise AttributeError(
-                f'access_mode is not one of ("r", "r+"), :{access_mode}'
-            )
-
-        if not path.exists():
-            if access_mode == "r":
-                self.close()
-                raise FileNotFoundError(
-                    f"""File can't be found, use access_mode='r+' if you wan to create it.
-                        Path: <{path.absolute()}>,
-                        Mode: <{"readOnly" if access_mode == "r" else "readWrite"}>,
-                        """
-                )
-
-            path.parent.mkdir(parents=True, exist_ok=True)
-            path.touch()
-
-        if not path.is_file():
-            self.close()
-            raise FileNotFoundError(
-                f"""path does not lead to a file: <{path.absolute()}>."""
-            )
-
-        self._acces_mode = access_mode
         self._kwargs = kwargs
         self.load()
+
+    def __new__(class_, path, *args, **kwargs):
+        h = hash(path)
+        if h in class_._paths:
+            raise AttributeError(
+                f'A BetterJSONStorage object already exists with path < "{path}" >'
+            )
+        class_._paths.append(h)
+        instance = object.__new__(class_)
+        setattr(instance, "_hash", h)
+        return instance
+
+    def __del__(self):
+        self.close()
 
     def __repr__(self):
         return (
@@ -112,7 +127,7 @@ class BetterJSONStorage(Storage, Singleton):
                 f.write(compress(dumps(self._handle, **self._kwargs)))
 
     def write(self, data: Mapping) -> None:
-        if not self._acces_mode == "r+":
+        if not self._access_mode == "r+":
             raise PermissionError("Storage is openend as read only")
         self._handle = data
         Thread.start_new_thread(self._write_async, ())
@@ -124,5 +139,5 @@ class BetterJSONStorage(Storage, Singleton):
             self._handle = None
 
     def close(self):
-        if (h := hash(self._path)) in (p := self.__class__._paths):
-            p.remove(h)
+        if self._hash in (p := self.__class__._paths):
+            p.remove(self._hash)
