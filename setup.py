e296e1c9a14f849c5032f89d620c47ba8cecb09a
Thomas-Howest <thomas.eeckhout@student.howest.be>
2022-01-29 21:50:17 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="BetterJSONStorage",
-    version="1.2",
+    version="1.2.1",
     author="Thomas Eeckhout",
     author_email="Thomas.Eeckhout@outook.be",
     description="An optimized tinyDB storage extension",
